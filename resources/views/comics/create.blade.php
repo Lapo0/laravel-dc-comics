66156d8da7c1eef30a8e393739fb6486065b34d0
Lapo0 <lapo.gini.01@gmail.com>
2023-04-14 17:25:17 +0200

    FEAT create view

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app');
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <h1>
+                create
+            </h1>
+
+            <form action="">
+                vuoto
+            </form>
+        </div>
+    </div>
+
+@endsection
