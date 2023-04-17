022782b06571e1d7b07921881433a983fc1aff4d
Lapo0 <lapo.gini.01@gmail.com>
2023-04-17 16:19:16 +0200

    FEAT create form

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,6 +4,14 @@
 
     <div class="container">
         <div class="row">
+            <a class="btn btn-primary" href="{{ route('comics.create') }}">
+                Nuovo Comic
+            </a>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
 
             <table class="table">
                 <thead>
